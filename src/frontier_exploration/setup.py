ca750322af1c1306e6e2c469fe109e4b6892c084
Daniel Agar <danieljagar@outlook.com>
2024-08-29 18:15:06 +0100

    Added custom environment

--- src/frontier_exploration/setup.py
+++ src/frontier_exploration/setup.py
@@ -15,6 +15,10 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob.glob(os.path.join("launch", "*launch.[pxy][yma]*")),
         ),
+        (
+            os.path.join("share", package_name, "worlds"),
+            glob.glob(os.path.join("worlds", "*.world"), recursive=True),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
